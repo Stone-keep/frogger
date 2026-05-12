b229ba3249c53e769da4784e4b576b50b8baba62
Stone-keep <radek12345@gmail.com>
2026-05-12 17:39:18 +0200

    Return to title screen on win and car collision

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,7 +5,7 @@ var game_timer := 0
 
 func _on_win_area_body_entered(body: Node2D) -> void:
 	if body == $Objects/Player:
-		print("Player has entered the area")
+		go_to_title(body)
 
 func _on_car_timer_timeout() -> void:
 	var car = car_scene.instantiate() as Area2D
@@ -15,8 +15,11 @@ func _on_car_timer_timeout() -> void:
 	car.connect("body_entered", go_to_title)
 
 func go_to_title(_body):
-	print("player crashed with a car")
+	call_deferred("change_scene")
+	
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
 
 func _game_timer_second() -> void:
 	game_timer += 1
-	$UI/CanvasLayer/TimePassed.text = "Time Passed: %s seconds" % game_timer
+	$HUD/CanvasLayer/TimePassed.text = "Time Passed: %s seconds" % game_timer
